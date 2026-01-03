15c1035bc088620ccd736f7421d6f8b99984a4d1
Thiago Nascimento <tiagolinsnasc@gmail.com>
2026-01-02 22:24:34 -0300

    Delete araci directory
    Diretório duplicado parojeto dentro do projeto

--- araci/scripts/habilitar_feroz.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-#Habilita feroz
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Araci":
-		#print("Feroz disponível")
-		Globals.pw_feroz_enabled()
--- araci/scripts/papers_read.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-var flag_on = true
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Araci" and flag_on:
-		var img = preload("res://n_assets/n_scenes/elements/journalist_image.png")
-		Globals.show_side_mensage("Uma foto! Essa mulher é uma conhecida jornalista engajada na causa ambiental.",img,10.0)		
-		flag_on = false
